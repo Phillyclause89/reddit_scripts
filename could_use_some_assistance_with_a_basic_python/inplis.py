d48191b1c6ee422c239716d9eb7bd59e1b7e0d03
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2019-12-08 04:21:01 -0800

    cleanup

--- could_use_some_assistance_with_a_basic_python/inplis.py
+++ could_use_some_assistance_with_a_basic_python/inplis.py
@@ -1,41 +1,32 @@
-from spyobject import SPyObject as SPY
+from spyobject import SPyObject as SPyO
 
-file1 = open(r"creditCards", "r")
-# while True:
-#     cardNum = file1.readline()
-#     print("raw", cardNum)
-#
-#     cardNum = cardNum.rstrip()
-#     print("striped", cardNum)
-#     print(len(cardNum))
-#
-#     if cardNum[0] == str(4) and len(cardNum) == 16:
-#
-#         print("This card is a Visa, #" + str(cardNum))
-#
-#     elif cardNum[0] == str(5) and len(cardNum) == 16:
-#
-#         print("This card is a Mastercard, #" + str(cardNum))
-#
-#     elif cardNum[0] == str(2) and len(cardNum) == 16:
-#
-#         print("This card is an American Express, #" + str(cardNum))
-#
-#     else:
-#
-#         print("Invalid Card.")
+file0 = open(r"creditCards.txt", "r")
+while True:
+    cardNum = file0.readline()
+    print("raw", cardNum)
+    cardNum = cardNum.rstrip()
+    print("striped", cardNum)
+    print(len(cardNum))
+    # Op's infinite loop can be broken by checking len(cardNum)
+    if len(cardNum):
+        file0.close()
+        break
 
-
-# Determining if a credit card is number is valid
+    if cardNum[0] == str(4) and len(cardNum) == 16:
+        print("This card is a Visa, #" + str(cardNum))
+    elif cardNum[0] == str(5) and len(cardNum) == 16:
+        print("This card is a Mastercard, #" + str(cardNum))
+    elif cardNum[0] == str(2) and len(cardNum) == 16:
+        print("This card is an American Express, #" + str(cardNum))
+    else:
+        print("Invalid Card.")
 
 try:
     file1 = open("creditCards.txt", "r")
-    S = SPY(file1, globals())
+    S = SPyO(file1, globals())
     print(S.attributes, S.obj_info())
-
 except FileNotFoundError:
     print("ERROR! Invalid File!")
-
 else:
     line1 = file1.readline()
     line2 = file1.readline()
@@ -48,23 +39,31 @@ else:
     line9 = file1.readline()
     line10 = file1.readline()
     line11 = file1.readline()
-    S = SPY(line1, globals())
+    S = SPyO(line1, globals())
     print(S.attributes, S.obj_info())
+    file1.close()
 
-    # while line:
-    #     cardNum = file1.readline()
-    #     cardNum = cardNum.rstrip()
-    #     SPY(cardNum, globals()).obj_info()
-    #     if len(cardNum) == 16:
-    #         if cardNum[0] == str(4):
-    #             print("This card is a Visa, #" + str(cardNum))
-    #         elif cardNum[0] == str(5):
-    #             print("This card is a Mastercard, #" + str(cardNum))
-    #         elif cardNum[0] == str(2):
-    #             print("This card is an American Express, #" + str(cardNum))
-    #     else:
-    #         print("Invalid Card.")
-    #     # if len(cardNum) == 0:
-    #     #     break
+try:
+    file2 = open("creditCards.txt", "r")
+    S = SPyO(file2, globals())
+    print(S.attributes, S.obj_info())
+except FileNotFoundError:
+    print("ERROR! Invalid File!")
+else:
+    while True:
+        cardNum = file2.readline()
+        cardNum = cardNum.rstrip()
+        SPyO(cardNum, globals()).obj_info()
+        if len(cardNum) == 16:
+            if cardNum[0] == str(4):
+                print("This card is a Visa, #" + str(cardNum))
+            elif cardNum[0] == str(5):
+                print("This card is a Mastercard, #" + str(cardNum))
+            elif cardNum[0] == str(2):
+                print("This card is an American Express, #" + str(cardNum))
+        else:
+            print("Invalid Card.")
+        if len(cardNum):
+            break
 
-file1.close()
+
