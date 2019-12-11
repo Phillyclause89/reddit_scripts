280deeb6d6580dce54052b3badff896294ac909a
Phil <45711864+Phillyclause89@users.noreply.github.com>
2019-12-11 12:16:55 -0800

    really need to move this into its own project

--- target_specific_window/test_window_refocus.py
+++ target_specific_window/test_window_refocus.py
@@ -6,14 +6,13 @@ import time
 class Test(TestCase):
     def test_bring_window_to_top(self):
         with self.assertRaises(window_refocus.WindowNotFoundError) as e:
-            window_refocus.bring_window_to_top("TEST FAILED!!!!!!!!!!!")
+            window_refocus.focus_on_window("TEST FAILED!!!!!!!!!!!")
 
         os.startfile(r"C:\WINDOWS\system32\notepad.exe")
         time.sleep(2)
-        os.startfile(r"C:\Users\Philip\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Command Prompt")
+        os.startfile(r"C:\Users\palees\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Command Prompt")
         time.sleep(2)
-        window_refocus.bring_window_to_top("Untitled - Notepad")
+        window_refocus.focus_on_window("Untitled - Notepad")
         time.sleep(2)
-        window_refocus.bring_window_to_top("Command Prompt")
-        print("pass")
+        window_refocus.focus_on_window("Command Prompt")
 
--- target_specific_window/window_refocus.py
+++ target_specific_window/window_refocus.py
@@ -6,6 +6,8 @@ import keyboard
 import win32gui
 import win32con
 import time
+import win32com.client as win32
+
 
 
 class WindowNotFoundError(Exception):
@@ -13,19 +15,14 @@ class WindowNotFoundError(Exception):
     pass
 
 
-class WindowNotSupportedError(Exception):
-    """Error to be raised if window name is not found"""
-    pass
-
-
-def bring_window_to_top(window_name):
+def focus_on_window(window_name):
     """Will bring all windows with a specific name to the top. The last one to be brought up will be in focus.
 
     Todo:
         * I feel like I could do more with this. Might make it its own module
 
     Examples:
-        >>> bring_window_to_top("Untitled - Notepad")
+        >>> focus_on_window("Untitled - Notepad")
 
     Args:
         window_name: (str) The name of the target window that you want to bring into focus
@@ -35,42 +32,45 @@ def bring_window_to_top(window_name):
     Raises:
         WindowNotFoundError(Exception) if window_name is not in the list
 
-        WindowNotSupportedError(Exception)
-
     """
-
-    DEBUG = {}
 
     def window_dict_handler(hwnd, top_windows):
         """Adapted from: https://www.blog.pythonlibrary.org/2014/10/20/pywin32-how-to-bring-a-window-to-front/
 
         """
-        # DEBUG
-        top_windows[hwnd] = win32gui.GetWindowText(hwnd)
+
+        if win32gui.IsWindowVisible(hwnd) and len(win32gui.GetWindowText(hwnd)) > 0:
+            top_windows[hwnd] = win32gui.GetWindowText(hwnd)
 
     tw = {}
     win32gui.EnumWindows(window_dict_handler, tw)
-    tw = {i: str(x) for i, x in tw.items() if len(x) > 0}
     for handle in tw:
         if tw[handle] == window_name:
             win32gui.ShowWindow(handle, win32con.SW_NORMAL)
             win32gui.BringWindowToTop(handle)
-            win32gui.SetForegroundWindow(handle)
+            try:
+                win32gui.SetForegroundWindow(handle)
+            except Exception as e:
+                if e.__str__() == "(0, 'SetForegroundWindow', 'No error message is available')":
+                    pass
+                else:
+                    raise e
+
             return None
-    raise WindowNotFoundError(f"WindowNotFoundError : '{window_name}' does not appear to be a window name in: {tw}")
+    raise WindowNotFoundError(f"WindowNotFoundError : The window name '{window_name}' does not appear to be an item in: {tw}")
 
 
 if __name__ == "__main__":
     # Example require fresh window instances of Notepad and Command Prompt
     # Example call to program with the name "Untitled - Notepad" (windows default for new notepad instance) to top
-    bring_window_to_top("Untitled - Notepad")
+    focus_on_window("Untitled - Notepad")
     # Safety sleep just in case our PC is slow to bring up the windowHello There!
     time.sleep(1)
     # Send our text into the window
     keyboard.write("Hello There!")
     # Example with CMD
     time.sleep(1)
-    bring_window_to_top(r"Command Prompt")
+    focus_on_window(r"Command Prompt")
     time.sleep(1)
     keyboard.write("python\nimport this\n")
 
